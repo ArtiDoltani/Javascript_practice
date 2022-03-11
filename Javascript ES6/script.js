4ae4330af7060f8c0755f86a23ddc49f8ccf31ed
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-03-11 23:38:22 +0500

    object literals

--- Javascript ES6/script.js
+++ Javascript ES6/script.js
@@ -18,5 +18,32 @@ console.log(
 
 
 )
-
 }
+
+//Spread Operators
+var mobiles = ["vivo", "Samsung","iPhone"];
+var homeElectronics=["TV","fridge"];
+var Electronics=["Desktop","laptop"];
+// Below it will create an array 
+var Electronics=["Desktop", mobiles,homeElectronics,"laptop"];
+// Below using 3 dots it will show all items in  single array
+//var Electronics=["Desktop", ...mobiles,...homeElectronics,"laptop"];
+
+console.log(Electronics);
+
+
+// <=====OBJECT LITERALS=====>
+var sound={
+    alarm(times){
+        console.log("Beep".repeat(times))
+    }, 
+    ringtone(times){
+        console.log("Song".repeat(times))
+    }}
+
+console.log(sound.alarm(3));
+console.log(sound.ringtone(2));
+
+
+
+
