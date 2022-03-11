9742726a29d3924c573fcda5eac2f18a5155cba1
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-03-11 22:55:29 +0500

    template string

---
+++ Javascript ES6/script.js
@@ -0,0 +1,22 @@
+// Template String
+function check(firstname)
+{
+    console.log(`Hello  ${firstname}  ${LastName}`);
+
+}
+check("Arti","Doltani");
+
+
+// Another function 
+function SendEmail(firstname)
+
+{
+    var shipping =240;
+console.log(
+`hi ${firstname} , thanku for purchasing,
+                Shipping= ${shipping}   `
+
+
+)
+
+}
