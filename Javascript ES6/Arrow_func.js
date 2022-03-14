71fb79a9f6fd3c0863e837736ea5f56ff499538f
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-03-14 14:15:44 +0500

    Arrow Function

---
+++ Javascript ES6/Arrow_func.js
@@ -0,0 +1,41 @@
+// Creating regular function
+// const  hello= function(){
+// console.log("This is a regular function");
+
+//  }
+//  hello();
+
+// Converting regular function into Arrow Function
+// const hello=()=>{
+//     console.log("Converted regular function into Arrow Function");
+    
+//      }
+//      hello();
+
+
+     //======> Function returning something
+// const hello=function(){
+//    // console.log("Function returning something")
+//   return "hello";
+//      }
+//      hello();
+
+//One liner do not need curly braces/return
+    // const hello=()=>"hello";
+      
+    //  console.log( hello());
+
+//function with arguments
+//const greet=name => "Good Morning " + name;
+//console.log(greet("Arti"));
+// here we use one parameter name but if we use multiple then we should use brackets.
+const greet=(name,LastName) => "Good Morning " + name + LastName;
+console.log(greet("Arti"," Doltani"));
+
+
+
+
+
+
+
+
