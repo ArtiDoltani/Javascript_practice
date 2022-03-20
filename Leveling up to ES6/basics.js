0c3fbd73b9d3caafee6727b9f6827969286b887a
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-03-20 20:12:05 +0500

    console commands

---
+++ Leveling up to ES6/basics.js
@@ -0,0 +1,36 @@
+// SCOPE
+//x;
+//temporal dead zone
+//const x=1;
+
+// ====> Console commands
+console.time("your code took")
+console.log("Hello World");
+// it is also used as calculator
+console.log(34+5)
+// when U want throw a warning
+console.warn("This is warning");
+// Create objects
+console.log(
+  {
+    ab: "this", age:9, 
+  }
+)
+// we can print it in table form as well
+console.table(
+  {
+    ab: "this", year:9, 
+  }
+)
+//console.clear();// it will clear all 
+console.timeEnd("your code took");
+console.error("this is an error")
+console.assert(565<189,'Age >189 is not possible')//it is used to check condition it is for an error.
+
+
+
+
+
+
+ 
+
